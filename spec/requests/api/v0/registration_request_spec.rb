208bc8c212e84262ee30e6256ad3173a863a4a3b
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-22 21:31:25 -0700

    Fix: fix typos and create sad path test for user registration

--- spec/requests/api/v0/registration_request_spec.rb
+++ spec/requests/api/v0/registration_request_spec.rb
@@ -25,5 +25,22 @@ RSpec.describe 'User registration' do
       expect(created_user_response[:data][:attributes]).to have_key (:api_key)
 
     end
+
+    it 'return error message when user info is invalid' do
+      user_params = {
+        "email": "creamontheinside@paintjob.com",
+        "password": "icecream",
+        "password_confirmation": "icecream23"
+      }
+
+      post '/api/v0/users', params: user_params.to_json, headers: { 'Content-Type' => 'application/json' }
+      
+      expect(response).to have_http_status(400)
+      
+      error_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_response).to be_a Hash
+      expect(error_response[:errors].first[:detail]).to eq ("Validation failed: Password confirmation doesn't match Password")
+    end
   end
 end
