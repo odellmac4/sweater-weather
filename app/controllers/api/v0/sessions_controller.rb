15e57765b4fdc405bf88542c6e64b45d6d9f6163
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-23 11:46:23 -0700

    Feat: return user id email and api_key when user passes in email and password of an existing user in the body of the request

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Api::V0::SessionsController < ApplicationController
+  def create
+    
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      render json: UserSerializer.new(user), status: 201
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
